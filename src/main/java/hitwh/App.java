3b92784a955879973626e4a05e74de8a96321412
newbee <14312503+newbeejzx@user.noreply.gitee.com>
2024-10-28 15:24:07 +0800

    修改

--- src/main/java/hitwh/App.java
+++ src/main/java/hitwh/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Hello hello = new Hello();
+        hello.hello();
     }
 }
